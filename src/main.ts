2c3b44004a8130bc331c45b7a46e4ae329fa1114
Do Hoerin <hellodhlyn@gmail.com>
2019-11-10 02:43:10 +0900

    Adding CORS configs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.YURI_CORS_ORIGIN || 'http://127.0.0.1:8080',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
