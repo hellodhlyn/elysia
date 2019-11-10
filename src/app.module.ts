30691a92717cd7f2124c79d89e71475649aad430
Do Hoerin <hellodhlyn@gmail.com>
2019-11-10 18:30:43 +0900

    Adding health check path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { PingController } from './controllers/ping.controller';
 import { CryptoController } from './controllers/v1/crypto.controller';
 import { ConverterService } from './services/crypto/converter.service';
 import { ExchangeService } from './services/crypto/exchanges.service';
 
 @Module({
   imports: [],
-  controllers: [CryptoController],
+  controllers: [PingController, CryptoController],
   providers: [ConverterService, ExchangeService],
 })
 export class AppModule {}
